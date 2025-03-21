9598bb87d7db1649f8f4b075430055f85cd2acda
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-03-21 00:44:03 -0400

    Mise à jour du fichier vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/site-portfolio/projets-react/SmartIDE",
+  base: "/site-portfolio/projets/projets-react/SmartIDE",
   plugins: [react()],
 });
